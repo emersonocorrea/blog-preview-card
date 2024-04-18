a2e3fab568ca0af3943c883cf828365637770a75
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2024-04-17 21:41:59 -0300

    finish

---
+++ src/scripts/tailwindcss.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        boxShadow: {
+            'card': ' 7px 7px 0px #000',
+            'cardMd': ' 14px 14px 0px #000'
+        }
+      }
+    }
+  }
