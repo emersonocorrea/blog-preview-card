3ee4e4f664ca956ca4fba224c64548d80a0264a2
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2024-04-19 23:10:33 -0300

    update

--- src/scripts/tailwindcss.js
+++ src/scripts/tailwindcss.js
@@ -1,6 +1,9 @@
 tailwind.config = {
     theme: {
       extend: {
+        colors: {
+          'primary': '#F4D04E'
+        },
         boxShadow: {
             'card': ' 7px 7px 0px #000',
             'cardMd': ' 14px 14px 0px #000'
